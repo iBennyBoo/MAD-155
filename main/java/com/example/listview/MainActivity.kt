93dbe2911aedce36fdd52cd656d259d1bceab59c
BennyBoo <bpb062004@gmail.com>
2023-08-29 19:44:34 -0500

    Add files via upload

---
+++ main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView1 = findViewById<ListView>(R.id.listView1)
+        val names = arrayOf("Shrek", "Shrek 2", "Shrek 3", "Shrek 4")
+
+        val myAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this,android.R.layout.simple_list_item_1, names
+        )
+
+        listView1.adapter = myAdapter
+
+        listView1.setOnItemClickListener(){ adapterView, view, i, l ->
+            Toast.makeText(this,"item is "+ names[i], Toast.LENGTH_LONG).show()
+        }
+    }
+}
