46b8e67624cb7fd44a0aad14bbf44bd567db903a
BennyBoo <bpb062004@gmail.com>
2023-09-20 16:52:44 -0500

    Add files via upload

---
+++ TabView/main/java/com/example/tabview/ui/settings/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.tabview.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tabview.R
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        settingsViewModel = ViewModelProvider(this).get(settingsViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+
+
+}
