51ba41e743f8dd488d978d1290eded8736a8ce64
BennyBoo <bpb062004@gmail.com>
2023-10-12 15:38:37 -0500

    Add files via upload

---
+++ makeItMove/main/java/com/example/motion/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.motion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_layout)
+        var view = R.layout.my_layout
+
+    }
+
+
+}
